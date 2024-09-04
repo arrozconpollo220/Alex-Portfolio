1168577dc9b00e08192d80d9abbab28f846d112b
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 17:26:44 -0600

    updated main and app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// Bringing in the required import from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import Nav from './components/NavTabs';
+
+function App() {
+  // Returning the Nav component and the Outlet component. The Outlet component is a placeholder for the child routes to be rendered.
+  return (
+    <>
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+        <footer>This is a footer</footer>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import ReactDOM from 'react-dom/client'
+// Importing the createBrowserRouter and RouterProvider functions from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Importing the App component and the Error, Home, Contact, About, Portfolio, and Resume pages
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+// Creating a new router using the createBrowserRouter function from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
