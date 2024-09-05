e25e469b25013668fae6f6ec8f51eeb94b3c97db
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 18:35:39 -0600

    updated resume.jsx file

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+      <div>
+        <h1>Resume</h1>
+        <p>
+This is the resume page. add your resume here.
+        </p>
+      </div>
+    );
+  }
