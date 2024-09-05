58d199bc9594b747eb2da35076f664fd7c80408a
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 18:34:43 -0600

    updated the portfolio.jsx file

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {
+    return (
+      <div>
+        <h1>Portfolio</h1>
+        <p>
+      This is the portfoloio page. list 3 projects here.
+        </p>
+      </div>
+    );
+  }
