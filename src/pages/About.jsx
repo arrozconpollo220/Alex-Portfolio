d172b429d68f55162ee1fd92439072733bae3b32
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 23:44:34 -0600

    updated about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,10 +3,9 @@ import Emoji2 from '../assets/2.svg'
 export default function About() {
     return (
       <div>
-        <h1>About Page</h1>
-        <p>
-         This is the about page. Write about yourself here.
-        </p>
+        <h1>About.</h1>
+       <h2>I'm a software engineer based in Salt Lake City Metropolitan area</h2>
+        <p>I have a passion for software development and love to create things that can help people. I have experience in building web applications using JavaScript, React, and Node.js. I am always looking for opportunities to learn and grow as a developer.</p>
         <img src={Emoji2} alt="Emoji of Alex" />
       </div>
     );
