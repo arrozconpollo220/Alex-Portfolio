23d2ea19b6a87de5eb9355104d59c93b08a9e14b
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-05 11:20:19 -0600

    added resume and text content to resume.jsx and update text content in about.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,8 @@ export default function About() {
         <h1>About.</h1>
        <h2>I'm a software engineer based in Salt Lake City Metropolitan area</h2>
         <p>I have a passion for software development and love to create things that can help people. I have experience in building web applications using JavaScript, React, and Node.js. I am always looking for opportunities to learn and grow as a developer.</p>
+
+        <p>When I'm not infront of the computer, I enjoying watching Football, listening to hiphop music, cooking with my family and traveling the world</p>
         <img src={Emoji2} alt="Emoji of Alex" />
       </div>
     );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,12 @@
+// importing resume from the public folder
+import Resume from '../assets/AR resume.jpg';
+
 export default function Resume() {
     return (
       <div>
         <h1>Resume</h1>
-        <p>
-This is the resume page. add your resume here.
-        </p>
+      <h2>Take a look at my resume and feel free to download for your convenience</h2>
+      <img src={Resume} alt="Resume" />
       </div>
     );
   }
