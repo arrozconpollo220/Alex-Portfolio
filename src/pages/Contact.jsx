5771df0abe3a1f24843e412ddb637d912c1c4c0b
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 18:21:14 -0600

    updated contact.jsx file

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+      <div>
+        <h1>Contact Me</h1>
+        <p>
+  this is the contact page. create a form here.
+        </p>
+      </div>
+    );
+  }
