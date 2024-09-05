b34cadb45893e075546c48c8dbf6f6b5b47266c6
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-05 11:21:23 -0600

    updated text content in resume.jsx file

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume() {
     return (
       <div>
         <h1>Resume</h1>
-      <h2>Take a look at my resume and feel free to download for your convenience</h2>
+      <h2>Take a look at my resume and feel free to download for your records</h2>
       <img src={Resume} alt="Resume" />
       </div>
     );
