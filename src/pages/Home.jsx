3c6977d0de26aabce7b45d5722dc363e55a73dbc
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 22:15:07 -0600

    updated home.jsx file. adjusted text content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import Emoji1 from '../assets/Portfolio Alex.svg'
 export default function Home() {
     return (
       <div>
-        <h1>Alexander Rodriguez</h1>
+        <h3>Hi, I'm</h3>
+        <h1>Alex</h1>
+        <h3>Software Engineer & full-Stack Web Developer.</h3>
         <p>
             This is the Home page. Add some text here.
         </p>
