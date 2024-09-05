a8890b4600c6da32c165d17b3c5f269da0f2d4dc
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-04 18:18:51 -0600

    updated experience.jsx file

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,10 @@
+export default function Experience() {
+    return (
+      <div>
+        <h1>Experience</h1>
+        <p>
+This is the Experience page. add dead face project here
+        </p>
+      </div>
+    );
+  }
