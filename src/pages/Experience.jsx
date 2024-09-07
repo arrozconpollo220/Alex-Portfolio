fbafcc1d25f4c7fce38b41c6d425d887b030e0e0
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-07 16:35:48 -0600

    added deadface project screenshot to experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,6 +1,9 @@
 // importing css file from the styles folder
 import '../styles/experience.css';
 
+// importing dead face project image from the images folder
+import deadFace from '../assets/deadface screenshot.png';
+
 export default function Experience() {
     return (
       <div>
@@ -8,6 +11,7 @@ export default function Experience() {
         <p>
 This is the Experience page. add dead face project here
         </p>
+        <img src= {deadFace} alt="dead face project" />
       </div>
     );
   }
