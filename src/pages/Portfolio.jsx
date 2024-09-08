244523529475f14ae6fde8013ab21f9c7081b6bd
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-07 18:35:45 -0600

    added portfolio images to portfolio.jsx

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,29 @@
 // importing css file from the styles folder
 import '../styles/portfolio.css';
 
+// importing images from the assets folder
+import slcEvent from '../assets/Screenshot of SLC Events.png';
+import weatherDashboard from '../assets/Screenshot Weather Dash.png';
+import noteTaker from '../assets/screenshot notetaker.png';
+
 export default function Portfolio() {
     return (
       <div>
         <h1>Portfolio</h1>
-        <p>
-      This is the portfoloio page. list 3 projects here.
-        </p>
+       <p>Write a paragraph</p>
+       <figure>
+        <figcaption>SLC Events</figcaption>
+          <img src= {slcEvent} alt="SLC Events" />   
+       </figure>
+       <figure>
+        <figcaption>Weather Dashboard</figcaption>
+          <img src= {weatherDashboard} alt="SLC Events" />   
+       </figure>
+       <figure>
+        <figcaption>Note Taker</figcaption>
+          <img src= {noteTaker} alt="SLC Events" />   
+       </figure>
+
       </div>
     );
   }
