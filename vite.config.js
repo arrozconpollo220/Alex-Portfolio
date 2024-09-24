639f47866b88c4feeea431d002917479b4689c41
alexander rodriguez <alexander.rodriguez2121@gmail.com>
2024-09-24 17:23:31 -0600

    updated Vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true,
+    open: process.env.NODE_ENV !== 'production', // Only open in development
   },
 });
+
